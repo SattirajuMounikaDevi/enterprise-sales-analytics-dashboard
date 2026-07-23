3b3d9950ff6c531311e1d364f378b9de0fc635d3
Mounika Devi Sattiraju <mounikasattiraju@gmail.com>
2026-07-23 23:47:15 +0530

    sixth Commit - changed the dependency & salesanalytics application.

--- src/main/java/com/project/salesanalytics/SalesAnalyticsApplication.java
+++ src/main/java/com/project/salesanalytics/SalesAnalyticsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SalesAnalyticsApplication{
+public class SalesAnalyticsApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(SalesAnalyticsApplication.class, args);
